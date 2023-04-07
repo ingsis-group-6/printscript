51ac14de5c8b89f5b5f4d8361c42cfe80ebe5d35
ricardolopezb <ricardolopezbonaguro@gmail.com>
2023-04-07 17:59:43 -0300

    fixed error messages line description

--- interpreter/src/main/kotlin/interpreter/implementation/DeclarationInterpreter.kt
+++ interpreter/src/main/kotlin/interpreter/implementation/DeclarationInterpreter.kt
@@ -11,9 +11,10 @@ class DeclarationInterpreter(
     override fun interpret(ast: AST) {
         ast as DeclarationAST
         val identifier = ast.getIdentifier()
-        if (identifier in symbolTable.keys) throw Exception("Variable ${ast.getIdentifier()} is already declared")
+        val currentLine = ast.getTokensInLine().first().row
+        if (identifier in symbolTable.keys) throw Exception("(Line $currentLine) - Variable ${ast.getIdentifier()} is already declared")
         val type = ast.getType()
         symbolTable[identifier] = Pair(type, null)
     }
-    object NIL
+
 }

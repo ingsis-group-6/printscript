448e4765a2bc0fab1322e587ff403106b7bb7fe2
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2023-04-19 13:14:19 -0300

    cli working

---
+++ app/src/main/kotlin/printscript/v1/cli/CLI.kt
@@ -0,0 +1,85 @@
+package printscript.v1.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import common.token.Token
+import common.token.TokenType
+import lexer.implementation.Lexer
+import printscript.v1.app.ExecuteFunction
+import printscript.v1.app.FormatFunction
+import printscript.v1.app.LinterFunction
+import printscript.v1.app.PrintscriptFunction
+import java.io.File
+import java.util.*
+
+class PrintScript : CliktCommand(help = "Printscript") {
+
+    override fun run() = Unit
+}
+class Run : CliktCommand(help = "Run a Printscript file") {
+
+    val sourceFile by argument(help = "The source file to run")
+    override fun run() {
+        CLIUtils.runAppWithFunction(File(sourceFile), ExecuteFunction())
+    }
+}
+
+class Lint : CliktCommand(help = "Lint a Printscript file") {
+
+    val sourceFile by argument(help = "The source file to lint")
+    val configFile by argument(help = "The config file to use")
+    override fun run() {
+        CLIUtils.runAppWithFunction(File(sourceFile), LinterFunction(configFile))
+    }
+}
+
+class Format : CliktCommand(help = "Format a Printscript file") {
+
+    val sourceFile by argument(help = "The source file to format")
+    val configFile by argument(help = "The config file to use")
+
+    override fun run() {
+        CLIUtils.runAppWithFunction(File(sourceFile), FormatFunction(sourceFile, configFile))
+    }
+}
+
+class CLIUtils {
+    companion object {
+
+        private fun runLexer(file: File) {
+            val lexer = Lexer()
+            lexer.extractTokensFromFile(file)
+        }
+
+        private fun getTokenFromStringRepresentation(input: String): Token {
+            val parts = input.substringAfter("(").dropLast(1).split(", ")
+            val orderId = parts[0].substringAfter("=").toInt()
+            val tokenType = TokenType.valueOf(parts[1].substringAfter("="))
+            val value = parts[2].substringAfter("=")
+            val row = parts[3].substringAfter("=").toInt()
+            val col = parts[4].substringAfter("=").toInt()
+
+            return Token(orderId, tokenType, value, row, col)
+        }
+
+        fun runAppWithFunction(file: File, function: PrintscriptFunction) {
+            runLexer(file)
+
+            val listOfTokensInLine = mutableListOf<Token>()
+            val scanner = Scanner(File("Tokens.txt"))
+
+            while (scanner.hasNextLine()) {
+                val token = getTokenFromStringRepresentation(scanner.nextLine())
+                listOfTokensInLine.add(token)
+
+                if (token.tokenType == TokenType.SEMICOLON) {
+                    function.execute(listOfTokensInLine)
+                    listOfTokensInLine.clear()
+                }
+                if (!scanner.hasNextLine() && token.tokenType != TokenType.SEMICOLON) {
+                    throw java.lang.Exception("There is a semicolon missing in the last line of the file")
+                }
+            }
+        }
+    }
+}

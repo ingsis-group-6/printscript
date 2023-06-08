ca8a4fd0a0e9eaabd99509258ba0f92dd310f8e0
ricardolopezb <ricardolopezbonaguro@gmail.com>
2023-06-08 14:58:42 -0300

    Implemented base indentation in Else Block ASTs

--- formatter/src/main/kotlin/formatter/implementations/Formatter.kt
+++ formatter/src/main/kotlin/formatter/implementations/Formatter.kt
@@ -1,7 +1,12 @@
 package formatter.implementations
 
 import common.ast.AST
-import common.ast.implementations.asts.*
+import common.ast.implementations.asts.AssignationAST
+import common.ast.implementations.asts.DeclarationAST
+import common.ast.implementations.asts.DeclarationAssignationAST
+import common.ast.implementations.asts.FunctionAST
+import common.ast.implementations.asts.ConditionalAST
+import common.ast.implementations.asts.EndOfFileAST
 import common.config.reader.formatter.FormatterRules
 import common.token.Token
 import common.token.TokenType
